e1ea8724b5144d1fd482cb3a0c2bb3983819be46
Nanda-143 <nandagopal8897@gmail.com>
2023-07-27 14:54:04 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-d={'key':'pulihora raja'}
+d={'key':'pulihora raja','age':23}
 def data_render(request):
     return render(request,'data_render.html',context=d)
