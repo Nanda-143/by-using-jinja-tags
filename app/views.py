58022949d305437cde77fb2e34a6ab9e7fb620a4
Nanda-143 <nandagopal8897@gmail.com>
2023-07-28 13:16:25 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-d={'a':1,'b':23}
+d={'a':100,'b':230,'c':50}
 def data_render(request):
     return render(request,'data_render.html',context=d)
